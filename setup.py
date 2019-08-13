b7325bee0e281b051ff5ba2fdc2a02b53bbdf184
Angela Enriquez <anenriqu@outlook.com>
2019-08-13 18:17:17 +0200

    Fixing requirement pyYAML

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from setuptools import setup
 
 setup(name='mrta_datasets',
       version='0.1.0',
+      install_requires=[
+            'numpy',
+            'pyYAML',
+      ],
       description='Datasets and structs for MRTA',
       author='Angela Enriquez Gomez',
       author_email='angela.enriquez@smail.inf.h-brs.de',
